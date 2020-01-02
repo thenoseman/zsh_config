e254898353ac02a3da562918f1122a80d46dbfd4
thenoseman <frank.schumacher.privat@googlemail.com>
2020-01-02 10:31:10 +0100

    window positioning redone

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,3 +1,5 @@
+local log = hs.logger.new('home','debug')
+
 -- ReloadConfiguration
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
@@ -12,41 +14,41 @@ spoon.ClipboardTool.paste_on_select = true
 spoon.ClipboardTool.show_in_menubar = false
 spoon.ClipboardTool:start()
 
--- Window movement
-function focusedWindow() 
-  local win = hs.window.focusedWindow()
-  local windowFrame = win:frame()
-  local screen = win:screen()
-  local screenFrame = screen:frame()
-  return win, windowFrame, screenFrame
-end
-
+-- Screen/ Window movement
 hs.window.animationDuration = 0
 
--- 2/3 screen size, anchor top left
+-- Notice that hammerspoon regards desktop = all screens combined = continguous X coordinates starting top left on primary screen
+local primaryDisplay = hs.screen.primaryScreen()
+local secondaryDisplay = hs.screen'Dell'
+local primaryScreenFrame = primaryDisplay:frame()
+local secondaryScreenFrame = secondaryDisplay:frame()
+
+-- 2/3 screen size, anchor top left, screen 1
 hs.hotkey.bind({"cmd", "shift"}, "1", function()
-  local win, windowFrame, screenFrame = focusedWindow()
-  windowFrame.x = 0
-  windowFrame.y = 0
-  windowFrame.w = (screenFrame.w / 3) * 2
-  windowFrame.h = screenFrame.h
-  win:setFrame(windowFrame)
+  local focusedWindow = hs.window.focusedWindow()
+  focusedWindow:move({x = 0, y = 0, w = 0.66, h = 1})
 end)
 
--- 1/3 screen size, anchor at 2/3 width
+-- 1/3 screen size, anchor at 2/3 width, screen 1
 hs.hotkey.bind({"cmd", "shift"}, "2", function()
-  local win, windowFrame, screenFrame = focusedWindow()
-  windowFrame.x = (screenFrame.w / 3) * 2
-  windowFrame.y = 0
-  windowFrame.w = screenFrame.w / 3
-  windowFrame.h = screenFrame.h
-  win:setFrame(windowFrame)
+  local focusedWindow = hs.window.focusedWindow()
+  focusedWindow:move({x = 0.66, y = 0, w = 0.34, h = 1})
+end)
+
+-- 1/3 screen size, anchor top left, screen 2
+hs.hotkey.bind({"cmd", "shift"}, "3", function()
+  local focusedWindow = hs.window.focusedWindow()
+  focusedWindow:move({x = secondaryScreenFrame.x, y = 0, w = secondaryScreenFrame.w / 3, h = secondaryScreenFrame.h})
+end)
+
+-- 2/3 screen size, anchor top right, screen 2
+hs.hotkey.bind({"cmd", "shift"}, "4", function()
+  local focusedWindow = hs.window.focusedWindow()
+  focusedWindow:move({x = secondaryScreenFrame.x + secondaryScreenFrame.w / 3, y = 0, w = (secondaryScreenFrame.w / 3) * 2, h = secondaryScreenFrame.h})
 end)
 
 -- Standard layout
 hs.hotkey.bind({"cmd", "shift"}, "9", function()
-  local primaryDisplay = hs.screen.primaryScreen()
-  local secondaryDisplay = hs.screen'Dell'
   local windowLayout = {
     {"Google Chrome", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1}, nil, nil},
     {"MacVim", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1}, nil, nil},
