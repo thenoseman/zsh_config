1a06fcdba87e7f18c7c37e22ede606853c6eeb4f
thenoseman <frank.schumacher.privat@gmail.com>
2020-04-29 10:05:24 +0200

    try another setting for MS Teams

--- home/.hammerspoon/msteams.lua
+++ home/.hammerspoon/msteams.lua
@@ -4,6 +4,7 @@
 local input_device = hs.audiodevice.defaultInputDevice()
 local menubar = nil
 local appname_for_trigger = "Microsoft Teams"
+local canvas = nil
 
 -- Mut/Unmute Mic
 function toggleInputMuted()
@@ -20,10 +21,13 @@ local teamsHotkey = hs.hotkey.new("", "escape", nil, toggleInputMuted, nil, nil)
 -- Install watcher for MS Teams
 --
 function applicationWatcher(appName, eventType)
-  -- App activated
+  -- App focused
   if (eventType == hs.application.watcher.activated) then
     if (appName == appname_for_trigger) then
       teamsHotkey:enable()
+      if canvas then
+        canvas:show()
+      end
     end
   end
 
@@ -38,6 +42,9 @@ function applicationWatcher(appName, eventType)
   if (eventType == hs.application.watcher.terminated) then
     if (appName == appname_for_trigger) then
       teamsHotkey:disable()
+      if canvas then
+        canvas:hide()
+      end
     end
   end
 end
@@ -52,28 +59,29 @@ end
 
 --- find the position of the microphon icon, takes a snapshot and shows it
 function showTeamsMuteState()
-  local log = hs.logger.new('zsh_config','debug')
-
   local snapWidth = 50 
   local snapHeight = 50 
   local app = hs.application.find(appname_for_trigger)
   local window = app:mainWindow()
   local frame = window:frame()
   local screen = window:screen()
-  local canvasX = (screen:currentMode().w  - snapWidth) / 2 
-  local canvasY = (screen:currentMode().h - snapHeight) / 2
+  local canvasX = screen:currentMode().w  - snapWidth - 10 
+  local canvasY = screen:currentMode().h - snapHeight - 10
   local areaRect = hs.geometry.rect(frame.x + (frame.w / 2) - 60, frame.y + frame.h - 180, snapWidth, snapHeight)
   local snapshot = screen:snapshot(areaRect)
 
-  local canvas = hs.canvas.new{x=canvasX, y=canvasY,h=snapHeight * 2,w=snapWidth * 2}:appendElements(
+  if canvas then
+    canvas:delete()
+  end
+
+  canvas = hs.canvas.new{x=canvasX, y=canvasY,h=snapHeight * 1.2 ,w=snapWidth * 1.2}:appendElements(
     {action = "fill", fillColor = { alpha = 0.5, green = 1.0  }, type = "rectangle", withShadow = true}, 
     {type = "image", image = snapshot, imageScaling = "none"}
     ):show()
-
-  hs.timer.doAfter(2, function() 
-    canvas:delete() 
-  end)
 end
 
-hs.hotkey.bind({"cmd", "alt", "shift"}, "1", showTeamsMuteState);
-
+hs.hotkey.bind({"cmd", "alt", "shift"}, "x", function()
+  if canvas then
+    canvas:delete()
+  end
+end);
