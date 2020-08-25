7d6b6a4a17b514817228b4a12ffea68a5faa047b
thenoseman <frank.schumacher.privat@googlemail.com>
2020-08-25 15:13:13 +0200

    use local ffmpeg

--- home/.zsh/tools/exif_rename.sh
+++ home/.zsh/tools/exif_rename.sh
@@ -2,14 +2,14 @@
 #
 # Rename images according to their exit ate tags
 #
-[ -e "sorted" ] && echo "The direcory 'sorted' already exists. Exiting" && exit 1
+[ -e "sorted" ] && echo "The directory 'sorted' already exists. Exiting" && exit 1
 
 mkdir sorted
 
 # Collect images
 find . -iname "*.jpg" -exec mv {} sorted \;
 
-cd sorted
+cd sorted || exit 1
 
 # Rotate images
 jhead -autorot "*.jpg"
@@ -18,11 +18,15 @@ jhead -autorot "*.jpg"
 exiftool -r "-FileName<CreateDate" -d "%Y/%Y-%m-%d/%Y_%m_%d_%H_%M_%S.%%e" .
 
 # Movies?
-for file in *.MOV; do mv "$file" "${file/.MOV}".mov; done
-ls *.m* > /dev/null
+for file in *.MOV; do 
+  mv "$file" "${file}.mov" 
+  mv "${file}.mov" "${file/.MOV}".mov
+done
+
+ls ./*.m* > /dev/null
 if [[ "$?" == 0 ]]; then
   for file in *.mov *.mp4; do
-    creation_date=$(~/Dropbox/Software/ffmpeg/ffmpeg -i "$file" 2>&1 | grep creation_time | head -n 1 | cut -d ":" -f 2,3,4 | sed 's/^//g')
+    creation_date=$(ffmpeg -i "$file" 2>&1 | grep creation_time | head -n 1 | cut -d ":" -f 2,3,4 | sed 's/^//g')
     date=$(echo $creation_date | cut -d " " -f 1 | sed 's/-/_/g')
     dateforfolder=$(echo $creation_date | cut -d " " -f 1)
     time=$(echo $creation_date | cut -d " " -f 2 | sed 's/:/_/g')
