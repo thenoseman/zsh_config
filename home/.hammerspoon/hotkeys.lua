32f2d5456de47ddfb8349f4a2e4c0c28e55f612d
thenoseman <frank.schumacher.privat@gmail.com>
2020-09-23 20:57:23 +0200

    fix ms teams ESC key in hammerspoon

--- home/.hammerspoon/hotkeys.lua
+++ home/.hammerspoon/hotkeys.lua
@@ -18,24 +18,3 @@ hs.hotkey.bind({"cmd", "shift"}, "ß", function()
   }
   log.i(hs.inspect.inspect(data))
 end)
-
--- shift + cmd + m
-k = hs.hotkey.modal.new('cmd-shift', 'm') 
-
--- Automatically exit modal after two seconds
-function k:entered()
-  hs.timer.doAfter(2, function()
-    k:exit()
-  end)
-end
-
--- ESC exits
-k:bind('', 'escape', function() 
-  k:exit() 
-end) 
-
--- ¯\_(ツ)_/¯  (S)
-k:bind('', 'S', nil, function() 
-  hs.eventtap.keyStrokes('¯\\_(ツ)_/¯')
-  k:exit()
-end)
--- home/.hammerspoon/msteams.lua
+++ home/.hammerspoon/msteams.lua
@@ -1,28 +1,15 @@
 --
 -- MS Teams mute by pressing ESCAPE
 --
--- local input_device = hs.audiodevice.defaultInputDevice()
-local menubar = nil
+local log = hs.logger.new('msteams.lua','debug')
 local appname_for_trigger = "Microsoft Teams"
-local canvas = nil
-local removeOverlayTimer = nil
 
--- Mut/Unmute Mic
-function toggleInputMuted()
-  hs.eventtap.event.newKeyEvent({"shift", "cmd"}, "m", true):post(hs.application.find(appname_for_trigger))
-  -- This is not needed since Teams now opens each conference in it's own window
-  -- hs.timer.doAfter(0.5, showTeamsMuteState)
- 
-  -- Automatically remove overlay after 5 minutes inactivity
-  -- removeOverlayTimer = hs.timer.doAfter(300, function()
-  --  canvas:delete()
-  --end)
-end
-
---
--- Hotkey
--- 
-local teamsHotkey = hs.hotkey.new("", "escape", nil, toggleInputMuted, nil, nil)
+-- Hotkey setup
+local teamsHotkey = hs.hotkey.new("", "escape", nil, function()
+  -- Must send to the "Helper"
+  local app = hs.appfinder.appFromName("Teams Helper")
+  hs.eventtap.event.newKeyEvent({"shift", "cmd"}, "m", true):post(app)
+end, nil, nil)
 
 --
 -- Install watcher for MS Teams
@@ -31,27 +18,17 @@ function applicationWatcher(appName, eventType)
   -- App focused
   if (eventType == hs.application.watcher.activated) then
     if (appName == appname_for_trigger) then
+      log.i("enable")
       teamsHotkey:enable()
-      if canvas then
-        canvas:show()
-      end
     end
   end
 
   -- App lost focus
-  if (eventType == hs.application.watcher.deactivated) then
-    if (appName == appname_for_trigger) then
-      teamsHotkey:disable()
-    end
-  end
-
   -- App terminated
-  if (eventType == hs.application.watcher.terminated) then
+  if (eventType == hs.application.watcher.deactivated or
+      eventType == hs.application.watcher.terminated) then
     if (appName == appname_for_trigger) then
       teamsHotkey:disable()
-      if canvas then
-        canvas:hide()
-      end
     end
   end
 end
@@ -63,33 +40,3 @@ appWatcher:start()
 if (hs.application.find(appname_for_trigger) ~= nil) then
   applicationWatcher(appname_for_trigger, hs.application.watcher.launched)
 end
-
---- find the position of the microphon icon, takes a snapshot and shows it
-function showTeamsMuteState()
-  local snapWidth = 50 
-  local snapHeight = 50 
-  local app = hs.application.find(appname_for_trigger)
-  local window = app:mainWindow()
-  local frame = window:frame()
-  local screen = window:screen()
-  local canvasX = screen:currentMode().w  - snapWidth - 10 
-  local canvasY = screen:currentMode().h - snapHeight - 10
-  local areaRect = hs.geometry.rect(frame.x + (frame.w / 2) - 85, frame.y + frame.h - 180, snapWidth, snapHeight)
-  local snapshot = screen:snapshot(areaRect)
-
-  if canvas then
-    canvas:delete()
-  end
-
-  canvas = hs.canvas.new{x=canvasX, y=canvasY,h=snapHeight * 1.2 ,w=snapWidth * 1.2}:appendElements(
-    {action = "fill", fillColor = { alpha = 0.5, green = 1.0  }, type = "rectangle", withShadow = true}, 
-    {type = "image", image = snapshot, imageScaling = "none"}
-    ):show()
-end
-
-hs.hotkey.bind({"cmd", "alt", "shift"}, "x", function()
-  if canvas then
-    canvas:delete()
-    canvas = nil
-  end
-end);
