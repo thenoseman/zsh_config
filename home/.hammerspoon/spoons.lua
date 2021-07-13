9482edc868f4d87ddfbc5f10bd8b48795778c3ad
thenoseman <frank.schumacher.privat@googlemail.com>
2021-07-13 11:48:34 +0200

    ignore large copy paste

--- home/.hammerspoon/spoons.lua
+++ home/.hammerspoon/spoons.lua
@@ -11,5 +11,5 @@ hs.loadSpoon("ClipboardTool")
 spoon.ClipboardTool:bindHotkeys( { show_clipboard = {{"cmd", "shift"}, "v"} })
 spoon.ClipboardTool.paste_on_select = true
 spoon.ClipboardTool.show_in_menubar = false
+spoon.ClipboardTool.max_size = true
 spoon.ClipboardTool:start()
-
