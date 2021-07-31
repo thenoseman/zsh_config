8c271b8ac4e5f15c8205bced8867a0aef2e1af95
thenoseman <frank.schumacher.privat@gmail.com>
2021-07-31 19:32:28 +0200

    remove ambient light control

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,10 +1,7 @@
-local log = hs.logger.new('zsh_config','debug')
-
 require "spoons"
 require "screen_layout"
 require "audio"
 require "msteams"
 require "hotkeys"
-require "ambient_lighting"
 require "mediakeys"
 
