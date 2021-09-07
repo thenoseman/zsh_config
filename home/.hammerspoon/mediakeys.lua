71aaccd91fcd7844e5040f47704c524f3b8b5884
thenoseman <frank.schumacher.privat@googlemail.com>
2021-09-07 11:30:29 +0200

    make mediakeys control CMUS if open

--- home/.hammerspoon/mediakeys.lua
+++ home/.hammerspoon/mediakeys.lua
@@ -1,4 +1,11 @@
-local log = hs.logger.new('mediakeys.lua','debug')
+local log = hs.logger.new('mediakeys','debug')
+local cmus_socket_path = hs.fs.pathToAbsolute("~/.config/cmus/socket")
+local cmus_remote_socket = hs.socket.new():connect(cmus_socket_path)
+local key_to_command = {
+  ["PLAY"] = "player-pause",
+  ["FAST"] = "player-next",
+  ["REWIND"] = "player-prev",
+}
 
 --
 -- Stop mediakeys to start apple apps
@@ -8,7 +15,17 @@ local tap = hs.eventtap.new({hs.eventtap.event.types.systemDefined}, function (e
   if data["key"] ~= "PLAY" and data["key"] ~= "FAST" and data["key"] ~= "REWIND" then
      return false, nil
   end
-  log.i("Ignoring mediakey " .. data["key"])
+
+  if not data["down"] then
+    if cmus_remote_socket == nil then
+      log.i("No socket present in " .. cmus_socket_path)
+      return true, nil
+    end
+
+    local command=key_to_command[data["key"]]
+    log.i("Sending command '" .. command .. "' to cmus socket")
+    cmus_remote_socket:send(command .. "\n")
+  end
   return true, nil
 end)
 tap:start()
