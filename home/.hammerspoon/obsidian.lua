5cf902afb5d06ac2fd6e4a424cebd055cc830f5f
thenoseman <frank.schumacher.privat@gmail.com>
2022-03-04 08:44:21 +0100

    don't delete obsidian icon

--- home/.hammerspoon/obsidian.lua
+++ home/.hammerspoon/obsidian.lua
@@ -1,11 +1,6 @@
 local status_line_bar = nil
 local obsidianBundleID = "md.obsidian"
 
-
-if status_line_bar ~= nil then
-  status_line_bar:delete()
-end
-
 status_line_bar = hs.menubar.new()
 status_line_bar:setTitle("🔘")
 
