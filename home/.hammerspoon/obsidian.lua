1fb017139bafe69532c7869726db16b5151870d5
thenoseman <frank.schumacher.privat@googlemail.com>
2022-03-18 08:58:10 +0100

    remove obsidian button

--- home/.hammerspoon/obsidian.lua
+++
@@ -1,9 +0,0 @@
-local status_line_bar = nil
-local obsidianBundleID = "md.obsidian"
-
-status_line_bar = hs.menubar.new()
-status_line_bar:setTitle("🔘")
-
-status_line_bar:setClickCallback(function()
-  hs.application.launchOrFocusByBundleID(obsidianBundleID)
-end)
