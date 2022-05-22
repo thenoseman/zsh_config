b3e2fa7f4ddd23487f0ba0d89815ac321a4303a8
thenoseman <frank.schumacher.privat@gmail.com>
2022-05-22 21:40:37 +0200

    remove unneccassars stuff

--- home/.zsh/modules/mute-sound-mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Mute sound
-# Acticate with
-# sudo defaults write com.apple.loginwindow LogoutHook $(pwd)/mute-sound-mac.sh
-if [[ "$1" == "unmute" ]]; then
-  osascript -e 'set volume without output muted'
-else
-  osascript -e 'set volume with output muted'
-fi
