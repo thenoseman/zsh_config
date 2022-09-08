88503c0d90210a87b58ed26cfeb45af64cb3cee4
thenoseman <frank.schumacher.privat@googlemail.com>
2022-09-08 12:48:55 +0200

    automatic icon change when ehadset is connected

--- home/.hammerspoon/audio.lua
+++ home/.hammerspoon/audio.lua
@@ -5,7 +5,48 @@ local log = hs.logger.new('audio.lua','debug')
 local headset_name = "Jabra Talk 30"
 local default_name = "Built-in Output"
 local connection_sound = hs.sound.getByFile(hs.fs.pathToAbsolute("~/.hammerspoon/gem.mp3"))
+local output_switcher_menubar = hs.menubar.new()
 
+--
+-- Create icon in menubar to quickly toggle between INTERNAl and HEADSET output
+--
+local volumes = { default=18, headset=18 }
+local icons = { default="🔈", headset="🎧" }
+
+local function output_switcher_menubar_set_title(device)
+  output_switcher_menubar:setTitle(icons[device])
+end
+
+local function output_switcher_menubar_clicked() 
+  local current_output_device_info = hs.audiodevice.current(false)
+  default = hs.audiodevice.findOutputByName(default_name)
+  headset = hs.audiodevice.findOutputByName(headset_name)
+
+  if current_output_device_info["name"] == default_name then
+    -- From DEFAULT -> HEADSET
+    volumes.default = default:outputVolume()
+    if headset ~= nil then
+      headset:setDefaultOutputDevice()
+      headset:setVolume(volumes.headset)
+      output_switcher_menubar_set_title("headset")
+    end
+  else
+    -- From HEADSET -> DEFAULT
+    if headset ~= nil then
+      volumes.headset = headset:outputVolume()
+    end
+    default:setDefaultOutputDevice()
+    default:setVolume(volumes.default)
+    output_switcher_menubar_set_title("default")
+  end
+end
+
+output_switcher_menubar_set_title("default")
+output_switcher_menubar:setClickCallback(output_switcher_menubar_clicked);
+
+--
+-- Switch do HEASET for input + output when connected
+--
 function onaudiodevicechange(event)
   if event == "dev#" then
     headset = hs.audiodevice.findDeviceByName(headset_name)
@@ -25,42 +66,11 @@ function onaudiodevicechange(event)
       hs.timer.doAfter(1, function() 
         connection_sound:play()
       end)
+
+      output_switcher_menubar_set_title("headset")
     end
   end
 end
 
 hs.audiodevice.watcher.setCallback(onaudiodevicechange)
 hs.audiodevice.watcher.start()
-
---
--- Create icon in menubar to quickly toggle between INTERNAl and HEADSET output
---
-local output_switcher = hs.menubar.new()
-local volumes = { default=18, headset=18 }
-
-local function output_switcher_clicked() 
-  local current_output_device_info = hs.audiodevice.current(false)
-  default = hs.audiodevice.findOutputByName(default_name)
-  headset = hs.audiodevice.findOutputByName(headset_name)
-
-  if current_output_device_info["name"] == default_name then
-    -- From DEFAULT -> HEADSET
-    volumes.default = default:outputVolume()
-    if headset ~= nil then
-      headset:setDefaultOutputDevice()
-      headset:setVolume(volumes.headset)
-      output_switcher:setTitle("🎧")
-    end
-  else
-    -- From HEADSET -> DEFAULT
-    if headset ~= nil then
-      volumes.headset = headset:outputVolume()
-    end
-    default:setDefaultOutputDevice()
-    default:setVolume(volumes.default)
-    output_switcher:setTitle("🔈")
-  end
-end
-
-output_switcher:setTitle("🔈")
-output_switcher:setClickCallback(output_switcher_clicked);
