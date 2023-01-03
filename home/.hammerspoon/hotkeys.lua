a2ab81c2c411bb0b31527ae322e08e5371508c2b
Frank Schumacher <frank.schumacher@telefonica.com>
2023-01-03 10:46:04 +0100

    show more info in window inspector

--- home/.hammerspoon/hotkeys.lua
+++ home/.hammerspoon/hotkeys.lua
@@ -4,17 +4,19 @@ local log = hs.logger.new("hotkey.lua", "debug")
 -- hammerspoon window inspector
 --
 hs.hotkey.bind({ "cmd", "shift" }, "ß", function()
-	local focusedWindow = hs.window.focusedWindow()
-	local data = {
-		window = {
-			id = focusedWindow:id(),
-			title = focusedWindow:title(),
-		},
-		application = {
-			name = focusedWindow:application():name(),
-			title = focusedWindow:application():title(),
-			bundleID = focusedWindow:application():bundleID(),
-		},
-	}
-	log.i(hs.inspect.inspect(data))
+  local focusedWindow = hs.window.focusedWindow()
+  local data = {
+    window = {
+      id = focusedWindow:id(),
+      title = focusedWindow:title(),
+      subrole = focusedWindow:subrole(),
+      role = focusedWindow:role(),
+    },
+    application = {
+      name = focusedWindow:application():name(),
+      title = focusedWindow:application():title(),
+      bundleID = focusedWindow:application():bundleID(),
+    },
+  }
+  log.i(hs.inspect.inspect(data))
 end)
