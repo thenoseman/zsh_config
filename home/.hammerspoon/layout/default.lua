c39d93aa91def471aff0fdc0b4a9a5e932a00819
Frank Schumacher <frank.schumacher@telefonica.com>
2023-01-10 10:49:32 +0100

    update default layout

--- home/.hammerspoon/layout/default.lua
+++ home/.hammerspoon/layout/default.lua
@@ -13,14 +13,14 @@ local M = {}
 -- Display to move to
 -- Coordinates table (x,y,w,h),
 M.windowLayout = {
-	{ "Brave Browser", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
-	{ "MacVim", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
-	{ "iTerm2", nil, primaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
-	{ "Brave Browser", "E-Mail", secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
-	{ "Microsoft Outlook", nil, secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
-	{ "Slack", nil, secondaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
-	{ "Obsidian", nil, secondaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
-	{ "Microsoft Teams", nil, secondaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
+  { "Brave Browser", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
+  { "MacVim", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
+  { "iTerm2", nil, primaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
+  { "Brave Browser", "E-Mail", secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
+  { "Microsoft Outlook", nil, secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
+  { "Slack", nil, secondaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
+  { "Obsidian", nil, secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
+  { "Microsoft Teams", nil, secondaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
 }
 
 return M
