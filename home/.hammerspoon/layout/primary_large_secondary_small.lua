8609d4f1aa9acb45984708cf839e6b89efdd827b
Frank Schumacher <frank.schumacher@telefonica.com>
2023-01-11 11:13:23 +0100

    layout for open DevTools

--- home/.hammerspoon/layout/primary_large_secondary_small.lua
+++ home/.hammerspoon/layout/primary_large_secondary_small.lua
@@ -20,6 +20,7 @@ M.windowLayout = {
   { "Slack", nil, secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
   { "Obsidian", nil, secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
   { "Microsoft Teams", nil, secondaryDisplay, { x = 0.25, y = 0, w = 0.75, h = 1 }, nil, nil },
+  { "Brave Browser", "DevTools -", secondaryDisplay, { x = 0, y = 0, w = 1, h = 1 }, nil, nil },
 }
 
 return M
