6f15bcc39da379c6f3ca7faef1df0abda9b4b152
thenoseman <frank.schumacher.privat@gmail.com>
2023-06-26 14:12:54 +0200

    generate binstubs for arm + intel

--- home/.zsh/tools/aws-vault-generate.sh
+++ home/.zsh/tools/aws-vault-generate.sh
@@ -1,17 +1,26 @@
+#!/usr/bin/env zsh
+#
+# Genrates aws-vault wrapper binstubs
+#
+set -e
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd -P)
+
 for bin in aws packer terraform; do
-  cat <<-EOF > $bin
+  cat <<-EOF > $script_dir/$bin
 #!/usr/bin/env zsh
 # vim: set ft=sh:
 set -e
 
+[[ -e "/opt/homebrew/bin" ]] && HOMEBREW_PREFIX="/opt/homebrew" || HOMEBREW_PREFIX="/usr/local"
+
 if [[ -n \$AWS_VAULT ]] || [[ -n \$VIMRUNTIME ]]; then
-  exec "/opt/homebrew/bin/$bin" "\$@"
+  exec "\$HOMEBREW_PREFIX/bin/$bin" "\$@"
 fi
 
 if [[ -z \$AWS_PROFILE ]]; then
-  exec "/opt/homebrew/bin/$bin" "\$@"
+  exec "\$HOMEBREW_PREFIX/bin/$bin" "\$@"
 fi
 
-exec aws-vault exec "\$AWS_PROFILE" -- "/opt/homebrew/bin/$bin" "\$@"
+exec aws-vault exec "\$AWS_PROFILE" -- "\$HOMEBREW_PREFIX/bin/$bin" "\$@"
 EOF
 done
