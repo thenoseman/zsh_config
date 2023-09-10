ab3e9d125d3a2af722221899ee7d1c771458c2fb
thenoseman <frank.schumacher.privat@gmail.com>
2023-09-10 14:14:39 +0200

    add README

--- home/.hammerspoon/libretranslate_terraform/output.tf
+++ home/.hammerspoon/libretranslate_terraform/output.tf
@@ -1,9 +1,16 @@
 output "libretranslate_api_base_url" {
-  value = local.libretranslate_api_url
+  description = "The base URl for API operations"
+  value       = local.libretranslate_api_url
+}
+
+output "libretranslate_api_swagger_docs_url" {
+  description = "The base URl for API operations"
+  value       = "${local.libretranslate_api_url}/docs"
 }
 
 output "translate_curl_example" {
-  value = <<-CURL
-  curl -X POST -H "Content-Type: application/json" --data '{"q":"it works","source":"auto","target":"de","format":"text"}' "${local.libretranslate_api_url}/translate"
+  description = "An example curl command to test your libre translate instance"
+  value       = <<-CURL
+  curl -X POST -H "Content-Type: application/json" --data '{"q":"it works","source":"auto","target":"de","format":"text"}' ${local.libretranslate_api_url}/translate
 CURL
 }
