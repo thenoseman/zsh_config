a2b7f3e90b1c33c4f10f4dbcfb6341524cf8c008
thenoseman <frank.schumacher.privat@gmail.com>
2023-12-13 10:42:54 +0100

    position outlook appointment notice

--- home/.hammerspoon/layout/primary_large_secondary_small.lua
+++ home/.hammerspoon/layout/primary_large_secondary_small.lua
@@ -21,6 +21,26 @@ M.windowLayout = {
   { "Obsidian", nil, secondaryDisplay, { x = 0, y = 0, w = 1, h = 1 }, nil, nil },
   { "Microsoft Teams", nil, secondaryDisplay, { x = 0.25, y = 0, w = 0.75, h = 1 }, nil, nil },
   { "Brave Browser", "DevTools -", secondaryDisplay, { x = 0, y = 0, w = 1, h = 1 }, nil, nil },
+  { "Microsoft Outlook", "Erinnerung", secondaryDisplay, { x = 0.7, y = 0.8, w = 0.29, h = 0.2 }, nil, string.match },
 }
 
 return M
+--[[
+ application = {
+    bundleID = "com.microsoft.Outlook",
+    name = "Microsoft Outlook",
+    pid = 91047,
+    title = "Microsoft Outlook",
+    visibleWindows = { <userdata 1> -- hs.window: 1 Erinnerung (0x6000002326b8), <userdata 2> -- hs.window: Posteingang • frank.schumacher@telefonica.com (0x600000232db8) }
+  },
+  window = {
+    id = 171,
+    role = "AXWindow",
+    size_h = 143.0,
+    size_w = 400.0,
+    subrole = "AXStandardWindow",
+    tabCount = 0,
+    title = "1 Erinnerung"
+  }
+}
+]]
