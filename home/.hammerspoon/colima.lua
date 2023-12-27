ac2b9e4c3f1f1032d763ed86f25f6ed33f4ddabb
thenoseman <frank.schumacher.privat@gmail.com>
2023-12-27 09:18:56 +0100

    Revert "remove colima menubar display"
    This reverts commit 82adf431a56422e0b4d1fbdbeaca02c487532d08.

---
+++ home/.hammerspoon/colima.lua
@@ -0,0 +1,38 @@
+local log = hs.logger.new("[colima]", "debug")
+local colimaPath = os.getenv("HOME") .. "/.colima"
+local colimaPathDocker = colimaPath .. "/default/daemon/daemon.pid"
+local menuBar = nil
+local icon = hs.image.imageFromPath("colima.png"):setSize({ w = 18, h = 18 })
+log.i("Watching " .. colimaPath .. " for changes")
+
+function colimaMenuBar()
+  menuBar = hs.menubar.new()
+  menuBar:setTitle("")
+  menuBar:setIcon(icon)
+end
+
+function colimaWatcher(paths, pathFlags)
+  --- docker socker changed
+  local index = hs.fnutils.indexOf(paths, colimaPathDocker)
+  if index then
+    if pathFlags[index]["itemRemoved"] then
+      -- Colima has stopped
+      log.i("colima has stopped")
+      if menuBar then
+        menuBar:delete()
+        menuBar = nil
+      end
+    elseif pathFlags[index]["itemCreated"] then
+      -- Colima has started
+      log.i("colima has started")
+      colimaMenuBar()
+    end
+  end
+end
+
+-- Startup check!
+if hs.fs.attributes(colimaPathDocker, "size") then
+  colimaMenuBar()
+end
+
+colimaPathWatcher = hs.pathwatcher.new(colimaPath, colimaWatcher):start()
