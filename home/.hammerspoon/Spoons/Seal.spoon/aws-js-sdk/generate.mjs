ebc9886b9c8fd45d7ec8efb83d300a48e284a239
thenoseman <frank.schumacher.privat@gmail.com>
2024-01-26 19:44:42 +0100

    better source detection for aws index

--- home/.hammerspoon/Spoons/Seal.spoon/aws-js-sdk/generate.mjs
+++ home/.hammerspoon/Spoons/Seal.spoon/aws-js-sdk/generate.mjs
@@ -4,14 +4,28 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const downloadUrl =
-  "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/_next/static/chunks/3711.88eee830d1c25cc8.js";
+const docsUrl = "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/";
+let response = await fetch(docsUrl);
+let body = await response.text();
+
+// src="/AWSJavaScriptSDK/v3/latest/_next/static/chunks/webpack-056504c57a44ca2b.js"
+const webpackUrl =
+  "https://docs.aws.amazon.com" +
+  body.match(/src="(\/AWSJavaScriptSDK\/v3\/latest\/_next\/static\/chunks\/webpack-.*?)"/)[1];
+response = await fetch(webpackUrl);
+body = await response.text();
+
+const a = body.match(/3711:"(.*?)"/);
+
+// "."+{3711:"a8380bcb31962806",4281:"a7acee2....
+// We need "3711".
+const downloadUrl = `https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/_next/static/chunks/3711.${body.match(/3711:"(.*?)"/)[1]}.js`;
 
 console.log("Generating searchindex from " + downloadUrl);
 
 // Download aws js sdk JS
-const response = await fetch(downloadUrl);
-let body = await response.text();
+response = await fetch(downloadUrl);
+body = await response.text();
 body = body.replace(/^.*JSON.parse\('/, "").replace(/'\).*$/, "");
 
 const awsSdk = JSON.parse(body);
