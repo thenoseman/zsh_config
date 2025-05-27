dd7303b1a3d525732338b7bd2a67864f4e623920
thenoseman <frank.schumacher.privat@gmail.com>
2025-05-27 11:08:56 +0200

    Layout for two bad displays next to each other

--- home/.hammerspoon/layout/primary_small_secondary_small.lua
+++ home/.hammerspoon/layout/primary_small_secondary_small.lua
@@ -16,10 +16,10 @@ M.windowLayout = {
   { "Brave Browser", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
   { "MacVim", nil, primaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
   { "iTerm2", nil, primaryDisplay, { x = 0.66, y = 0, w = 0.34, h = 1 }, nil, nil },
-  { "Microsoft Outlook", nil, secondaryDisplay, { x = 0, y = 0, w = 1, h = 1 }, nil, nil },
+  { "Microsoft Outlook", nil, secondaryDisplay, { x = 0, y = 0, w = 0.75, h = 1 }, nil, nil },
   { "Slack", nil, secondaryDisplay, { x = 0, y = 0, w = 0.66, h = 1 }, nil, nil },
   { "Obsidian", nil, secondaryDisplay, { x = 0, y = 0, w = 1, h = 1 }, nil, nil },
-  { "Microsoft Teams", nil, secondaryDisplay, { x = 0.19, y = 0, w = 0.81, h = 1 }, nil, nil },
+  { "Microsoft Teams", nil, secondaryDisplay, { x = 0.5, y = 0, w = 0.5, h = 1 }, nil, nil },
   { "Brave Browser", "DevTools -", secondaryDisplay, { x = 0, y = 0, w = 1, h = 1 }, nil, nil },
   { "Microsoft Outlook", "Erinnerung", secondaryDisplay, { x = 0.7, y = 0.8, w = 0.29, h = 0.2 }, nil, string.match },
 }
